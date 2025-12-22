65ede69aab33d349c91af4dea418c6eac8eeddee
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2025-12-22 13:27:34 +0000

    Sync LeetCode submission Runtime - 1 ms (99.95%), Memory - 77.5 MB (19.68%)

---
+++ raw-submissions/0053-maximum-subarray/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int len = nums.length;
+        int currSum=nums[0];
+        int maxSum=nums[0];
+        for(int i=1;i<len;i++){
+         
+            currSum = Math.max(nums[i] , currSum + nums[i]);
+            maxSum=Math.max(currSum,maxSum);
+            
+        }
+        return maxSum;
+    }
+}

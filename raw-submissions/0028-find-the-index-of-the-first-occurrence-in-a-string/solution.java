7d4d93156e4c6dcffb3dea96504a3dee1ebc6712
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2025-12-31 09:28:13 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.9 MB (82.84%)

---
+++ raw-submissions/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        // StringBuilder sb = new StringBuilder(haystack);
+        // for(char c : haystack.toCharArray()){
+        //     if(needle.charAt(c) != haystack.charAt)
+        //         return -1;
+        // }
+        // return true;
+        return haystack.indexOf(needle);
+    }
+}

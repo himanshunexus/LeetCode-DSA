d07f309dadd85c8fe9f16a16e1fbaf0ca525b8e6
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-01-05 13:58:05 +0000

    Sync LeetCode submission Runtime - 29 ms (43.79%), Memory - 46.9 MB (58.77%)

---
+++ raw-submissions/0387-first-unique-character-in-a-string/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        } 
+        for (int i = 0; i < n; i++) {
+            if (map.get(arr[i]) == 1)
+                return i;
+        }
+        return -1;
+    }
+}

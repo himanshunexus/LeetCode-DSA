de72ca4235b9f16c2d7994615ad7942520bba5b4
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-01-05 17:50:36 +0000

    Sync LeetCode submission Runtime - 1 ms (99.92%), Memory - 62.1 MB (89.38%)

---
+++ raw-submissions/0209-minimum-size-subarray-sum/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int len = nums.length;
+        int sum=0;
+        int left=0;
+        int minLen=Integer.MAX_VALUE;
+        for(int right=0;right<len;right++){
+            sum+=nums[right];
+            while (sum >= target) {
+                minLen = Math.min(minLen, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+        }
+        return minLen == Integer.MAX_VALUE ? 0 : minLen;
+    }
+}

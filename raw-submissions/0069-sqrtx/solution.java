0839365bf9dbbfa17328d4c857ac796160f5402c
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-01-06 08:01:52 +0000

    Sync LeetCode submission Runtime - 1 ms (97.73%), Memory - 42.6 MB (62.22%)

--- raw-submissions/0069-sqrtx/solution.java
+++ raw-submissions/0069-sqrtx/solution.java
@@ -1,11 +1,17 @@
 class Solution {
-    public int mySqrt(int x) {
-        if (x < 2) return x;
-        long r = x;
-        while (r * r > x) {
-            r = (r + x / r) / 2;
+    public int mySqrt(int n) {
+        int low =0;
+        int high =n;
+        while(low<=high){
+            long mid = (low+high)/2;
+            if(mid * mid == n)
+                return (int)mid;
+            else if(mid*mid > n)
+                high =  (int)mid-1;
+            else 
+                low =  (int)mid+1;
         }
-        return (int) r;
+        return (int)high;
     }
 }
 

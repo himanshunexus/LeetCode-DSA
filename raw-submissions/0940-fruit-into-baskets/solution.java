1a1592dd6dd0d52d14bb638e8b2dbf5ff803ef68
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-01-08 10:46:31 +0000

    Sync LeetCode submission Runtime - 52 ms (84.23%), Memory - 70.4 MB (56.95%)

---
+++ raw-submissions/0940-fruit-into-baskets/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int left = 0;
+        int n = fruits.length;
+        int maxLen =0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int right = 0; right < n; right++){
+            map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
+            while(map.size()>2){
+                map.put(fruits[left],map.get(fruits[left])-1);
+
+            if(map.get(fruits[left])==0)
+                map.remove(fruits[left]);
+            left++;
+            }
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+}

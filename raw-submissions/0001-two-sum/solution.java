d0ff780710916fc9f55a43b8050c1ce37063e1ab
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-01-12 04:47:05 +0000

    Sync LeetCode submission Runtime - 3 ms (58.78%), Memory - 47.2 MB (23.72%)

--- raw-submissions/0001-two-sum/solution.java
+++ raw-submissions/0001-two-sum/solution.java
@@ -3,10 +3,10 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
+            int res = target - nums[i];
 
-            if (map.containsKey(complement)) {
-                return new int[]{ map.get(complement), i };
+            if (map.containsKey(res)) {
+                return new int[]{ map.get(res), i };
             }
 
             map.put(nums[i], i);

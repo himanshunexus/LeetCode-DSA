8c24593f85c9ba6464629ecf9920c12f7441354f
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-01-14 15:20:10 +0000

    Sync LeetCode submission Runtime - 1 ms (95.86%), Memory - 46.8 MB (62.73%)

---
+++ raw-submissions/0102-binary-tree-level-order-traversal/solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        levelOrder(root,0,ans);
+        return ans;
+    }
+    private void levelOrder(TreeNode root,int level , List<List<Integer>> ans){
+        if(root == null) return;
+        if(ans.size()<= level) ans.add(new ArrayList<>());
+        ans.get(level).add(root.val);
+        levelOrder(root.left,level+1,ans);
+        levelOrder(root.right,level+1,ans);
+
+    }
+}

87c7800b2a251d8850c7543fe9bbf564c2a431c4
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-01-21 17:04:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.6 MB (87.97%)

---
+++ raw-submissions/0998-check-completeness-of-a-binary-tree/solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int countNodes(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+    private boolean isComplete(TreeNode root, int index, int totalNodes) {
+        if (root == null) return true;
+        if (index > totalNodes) return false;
+
+        return isComplete(root.left, 2 * index, totalNodes)
+            && isComplete(root.right, 2 * index + 1, totalNodes);
+    }
+    public boolean isCompleteTree(TreeNode root) {
+        int totalNodes = countNodes(root);
+        return isComplete(root, 1, totalNodes);
+    }
+}

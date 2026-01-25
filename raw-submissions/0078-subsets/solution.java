2298de17c1bac23b0e865153801b2fbb8a6fccd1
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-01-25 02:56:13 +0000

    Sync LeetCode submission Runtime - 1 ms (86.65%), Memory - 44.5 MB (6.95%)

---
+++ raw-submissions/0078-subsets/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        int n = nums.length;
+        int m = (1<<n);
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0; i<m; i++){
+            List<Integer> helper = new ArrayList<>();
+            for(int j = 0; j<n;j++){
+                if((i>>j) %2 == 1)
+                    helper.add(nums[j]);
+            }
+            ans.add(helper);
+        }
+        return ans;
+    }
+}

c14029417562785f4e5d41796fc95770aa350760
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-01-31 07:13:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 46.1 MB (88.31%)

---
+++ raw-submissions/0745-find-smallest-letter-greater-than-target/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int n = letters.length;
+        int low = 0;
+        int high = n-1;
+        char ans = letters[0];
+        while( low <= high){
+            int mid = low + (high - low)/2;
+            if (letters[mid] > target) {
+                ans = letters[mid];
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+}

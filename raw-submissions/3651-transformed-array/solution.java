37120f1ab02907c7ed3a519c6ef6c91b443efb59
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-02-05 02:41:43 +0000

    Sync LeetCode submission Runtime - 1 ms (98.46%), Memory - 46.6 MB (77.48%)

---
+++ raw-submissions/3651-transformed-array/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        
+        for(int i=0; i<n; i++){
+            int newIdx = (i + nums[i]) % n;
+            if(newIdx < 0){
+                newIdx += n;
+            }
+            result[i] = nums[newIdx];
+        }
+        return result;
+    }
+}

cda56ab7e4f68cafd88bcf4e5951e617484d578e
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-02-25 13:04:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.3 MB (15.72%)

--- raw-submissions/0100-same-tree/solution.java
+++ raw-submissions/0100-same-tree/solution.java
@@ -14,10 +14,21 @@
  * }
  */
 class Solution {
+    private void preorder(TreeNode var,ArrayList<Integer> list){
+        if(var == null){
+            list.add(null);
+            return;
+        }
+        list.add(var.val);
+        preorder(var.left,list);
+        preorder(var.right,list);
+        
+    }
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null && q==null) return true;
-        if(p==null || q==null) return false;
-        if(p.val != q.val) return false;
-        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        ArrayList<Integer> listOne = new ArrayList<>();
+        ArrayList<Integer> listTwo = new ArrayList<>();
+        preorder(p,listOne);
+        preorder(q,listTwo);
+        return listTwo.equals(listOne);
     }
 }

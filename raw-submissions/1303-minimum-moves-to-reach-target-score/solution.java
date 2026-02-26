73fcde88c899305a003509896d395e26225c0860
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-02-26 04:27:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.5 MB (8.65%)

---
+++ raw-submissions/1303-minimum-moves-to-reach-target-score/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minMoves(int target, int maxDoubles) {
+        int moves = 0;
+        while(target > 1){
+            if(maxDoubles == 0){
+                return moves + (target - 1);
+            }
+            if(target % 2 == 0){
+                target /= 2;
+                maxDoubles--;
+            }
+            else{
+                target--;
+            }
+            moves++;
+        }
+        return moves;
+    }
+}

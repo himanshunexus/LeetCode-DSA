eb229406fe7de330a7a30c5d77fb388f0cff6f42
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-02-26 03:16:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.7 MB (70.34%)

---
+++ raw-submissions/1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numSteps(String s) {
+        int count = 0;
+        int carry = 0;
+        for(int i = s.length() -1; i > 0; i--){
+            int bit = s.charAt(i) - '0';
+            if(bit + carry == 1){
+                count += 2;
+                carry = 1;
+            }
+            else{
+                count += 1;
+            }
+        }
+        return count + carry;
+    }
+}

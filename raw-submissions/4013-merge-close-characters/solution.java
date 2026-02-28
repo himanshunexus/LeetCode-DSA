9d9baf5494d37376167570d837f0c0bfcfa1c847
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-02-28 15:11:09 +0000

    Sync LeetCode submission Runtime - 7 ms (32.79%), Memory - 44.8 MB (28.96%)

---
+++ raw-submissions/4013-merge-close-characters/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String mergeCharacters(String s, int k) {
+        StringBuilder sb = new StringBuilder(s);
+        while(true){
+            boolean merged = false;
+            for(int i = 0; i < sb.length(); i++){
+                for(int j = i + 1; j < sb.length() && j-i <= k; j++){
+                    if(sb.charAt(i)== sb.charAt(j)){
+                        sb.deleteCharAt(j);
+                        merged = true;
+                        break;
+                    }
+                }
+                if(merged)
+                    break;
+            }
+            if(!merged)
+                break;
+        }
+        return sb.toString();
+    }
+}

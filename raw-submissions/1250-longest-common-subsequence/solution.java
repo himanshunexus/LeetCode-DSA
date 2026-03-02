fd3ab3cfe8c84b64735a9e05469a7f60bb639287
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-03-02 16:51:52 +0000

    Sync LeetCode submission Runtime - 34 ms (6.21%), Memory - 55.9 MB (5.21%)

---
+++ raw-submissions/1250-longest-common-subsequence/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    static int[][] res;
+    private int lcs(int i,int j,StringBuilder a , StringBuilder b){
+        if(i < 0 || j < 0) return 0;
+        if(res[i][j] != -1) return res[i][j];
+        if(a.charAt(i) == b.charAt(j))
+            return res[i][j] = 1 + lcs(i-1,j-1,a,b);
+        else
+            return res[i][j] = Math.max(lcs(i-1,j,a,b),lcs(i,j-1,a,b));
+    }
+    public int longestCommonSubsequence(String text1, String text2) {
+        StringBuilder a = new StringBuilder(text1);
+        StringBuilder b = new StringBuilder(text2);
+        int m = a.length() , n = b.length();
+        res = new int[m][n];
+        for(int i = 0; i < res.length; i++){
+            for(int j = 0; j < res[0].length; j++){
+                res[i][j] = -1;
+            }
+        }
+        return lcs(m-1,n-1,a,b);
+    }
+}

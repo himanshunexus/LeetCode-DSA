ffba05996412addfe70d0b0af92010f8b1cfd388
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-03-08 08:12:07 +0000

    Sync LeetCode submission Runtime - 4 ms (82.75%), Memory - 46.5 MB (34.98%)

--- raw-submissions/0242-valid-anagram/solution.java
+++ raw-submissions/0242-valid-anagram/solution.java
@@ -1,45 +1,12 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-    //     TreeMap<Character,Integer> map = new TreeMap<>();
-    //     for(int i=0;i<s.length();i++){
-    //         if(map.get(s.charAt(i))==null){
-    //             map.put(s.charAt(i),1);
-    //         }
-    //         else{
-    //             map.put(s.charAt(i),map.get(s.charAt(i))+1);
-    //         }
-    //     }
-    //         TreeMap<Character,Integer> map1 = new TreeMap<>();
-    //         for(int j=0;j<t.length();j++){
-    //             if(map1.get(t.charAt(j))==null){
-    //                 map1.put(t.charAt(j),1);
-    //         }
-    //             else{
-    //                 map1.put(t.charAt(j),map1.get(t.charAt(j))+1);
-    //         }
-           
-    //     }
-    
-    //    if (map.equals(map1)) {
-    //         return true;
-    //     } else {
-    //         return false;
-        
-    //     }
     if(s.length()!=t.length()){
         return false;
-    }
+        }
     char[] sArray=s.toCharArray();
     char[] tArray=t.toCharArray();
     Arrays.sort(sArray);
     Arrays.sort(tArray);
-
-    if (Arrays.equals(sArray, tArray)) {
-        return true;
-    } else {
-        return false;
-}
-            
+    return (Arrays.equals(sArray, tArray));
     }
-
 }

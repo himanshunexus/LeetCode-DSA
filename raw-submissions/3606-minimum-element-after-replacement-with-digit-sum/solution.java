b787a98b334ab4e49dc1a68346f55db05c7d8fb2
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-03-13 03:06:03 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 44.7 MB (74.23%)

---
+++ raw-submissions/3606-minimum-element-after-replacement-with-digit-sum/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minElement(int[] nums) {
+        int sum;
+        // Arrays.sort(nums);
+        for(int i = 0; i < nums.length; i++){
+            sum = 0;
+            int digit = nums[i];
+            while(digit > 0){
+                int rem = digit % 10;
+                sum += rem;
+                digit /= 10;
+            }
+            nums[i] = sum;
+        }
+        int res = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            res = Math.min(res , nums[i]);
+        }
+        return res;
+    }
+}

8aaa6f4ad9c89afc7ff5af63fbcce67cddab009a
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-03-21 04:18:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.9 MB (8.27%)

--- raw-submissions/4245-count-commas-in-range/solution.java
+++ raw-submissions/4245-count-commas-in-range/solution.java
@@ -1,14 +1,6 @@
 class Solution {
     public int countCommas(int n) {
-        long res = 0;
-        long start = 1000;
-        long comma = 1;
-        while(start <= n){
-            long end = Math.min(n , start * 1000 - 1);
-            res += (end - start + 1) * comma;
-            start *= 1000;
-            comma++;
-        }
-        return (int)res;
+        if( n < 1000) return 0;
+        return n - 999;
     }
 }

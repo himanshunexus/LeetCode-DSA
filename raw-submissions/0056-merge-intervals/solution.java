498c6e3b3befad98c75de62adfd24d2f685f08fd
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-03-22 04:47:00 +0000

    Sync LeetCode submission Runtime - 8 ms (91.60%), Memory - 49.2 MB (43.35%)

---
+++ raw-submissions/0056-merge-intervals/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        int n = intervals.length;
+        int[][] res = new int[n][2];
+        int idx = 0;
+        res[0] = intervals[0];
+        for (int i = 1; i < n; i++) {
+            if (res[idx][1] < intervals[i][0]) {
+                idx++;
+                res[idx] = intervals[i];
+            } else {
+                res[idx][1] = Math.max(res[idx][1], intervals[i][1]);
+            }
+        }
+        return Arrays.copyOf(res, idx + 1);
+    }
+}
+
+

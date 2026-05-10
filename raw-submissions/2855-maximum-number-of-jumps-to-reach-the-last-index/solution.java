74a26b997791e505570dc143a4399660ad665734
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-05-10 05:16:31 +0000

    Sync LeetCode submission Runtime - 13 ms (86.11%), Memory - 46.7 MB (91.22%)

---
+++ raw-submissions/2855-maximum-number-of-jumps-to-reach-the-last-index/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maximumJumps(int[] nums, int target) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        for (int i = 1; i < n; i++) {
+            dp[i] = -1;
+        }
+        dp[0] = 0;
+
+        for (int j = 1; j < n; j++) {
+            for (int i = 0; i < j; i++) {
+                if (dp[i] != -1) {
+                    long diff = (long)nums[j] - nums[i];
+                    if (diff >= -target && diff <= target) {
+                        dp[j] = Math.max(dp[j], dp[i] + 1);
+                    }
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+}

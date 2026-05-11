afb163fc23590a827e6ef385325164c3fb78fd28
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-05-11 02:55:02 +0000

    Sync LeetCode submission Runtime - 2 ms (96.86%), Memory - 46.8 MB (31.28%)

--- raw-submissions/2639-separate-the-digits-in-an-array/solution.java
+++ raw-submissions/2639-separate-the-digits-in-an-array/solution.java
@@ -1,19 +1,10 @@
 class Solution {
     private int size(int[] nums){
-        // int x=0;
-        int count=0;
-        // while(nums[i]!=0){
-        //     int digit = nums[i];
-        //     // x = digit%10;
-        //     x/=10;
-        //     count++;
-        // }
-        for(int i:nums)
-        {
-            while(i>0)
-            {
+        int count = 0;
+        for(int i : nums){
+            while(i > 0){
                 count++;
-                i/=10;
+                i /= 10;
             }
         }
         return count;
@@ -21,12 +12,12 @@ class Solution {
     public int[] separateDigits(int[] nums) {
         int n = size(nums);
        int[] ans = new int[n];
-       for(int i = nums.length-1; i>=0; i--){
+       for(int i = nums.length - 1; i >= 0; i--){
             int x = nums[i];
-            while(x>0){
+            while(x > 0){
                 ans[n-1] = x % 10;
                 n--;
-                x/=10;
+                x /= 10;
             }
        }
         return ans;

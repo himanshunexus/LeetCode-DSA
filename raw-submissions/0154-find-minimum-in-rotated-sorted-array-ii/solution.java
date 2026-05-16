2c205f18813911c0130fc750a7d79d1bf335d5c7
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-05-16 02:21:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.8 MB (57.23%)

--- raw-submissions/0154-find-minimum-in-rotated-sorted-array-ii/solution.java
+++ raw-submissions/0154-find-minimum-in-rotated-sorted-array-ii/solution.java
@@ -1,9 +1,20 @@
 class Solution {
     public int findMin(int[] nums) {
-       int min = Integer.MAX_VALUE;
-       for(int i = 0; i < nums.length; i++){
-            min = Math.min(min,nums[i]);
-       } 
-       return min;
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] > nums[r]) {
+                l = mid + 1;
+            } else if (nums[mid] < nums[r]) {
+                r = mid;
+            } else {
+                r--;
+            }
+        }
+
+        return nums[l];
     }
 }

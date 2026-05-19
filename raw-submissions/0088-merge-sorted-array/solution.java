4db88f9f31d549bca98fd6bd4aa8024a8a99be4a
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-05-19 12:52:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44 MB (16.58%)

--- raw-submissions/0088-merge-sorted-array/solution.java
+++ raw-submissions/0088-merge-sorted-array/solution.java
@@ -1,20 +1,34 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int ptr1 = m-1;
-        int ptr2 = n-1;
-        int k = m+n-1;
+        int left = 0, right = 0;
+        int[] res = new int[n + m];
+        int i = 0; 
 
-        while(ptr2 >=0){
-            if(ptr1 >= 0 && nums1[ptr1]>nums2[ptr2]){
-            nums1[k] = nums1[ptr1];
-            ptr1--;
-            k--;
+        while (left < m && right < n) {
+            if (nums1[left] <= nums2[right]) {
+                res[i] = nums1[left]; 
+                left++;
+            } else {
+                res[i] = nums2[right]; 
+                right++;
             }
-        else{
-            nums1[k] = nums2[ptr2];
-            ptr2--;
-            k--;
-            }
+            i++; 
+        }
+        
+        while (left < m) {
+            res[i] = nums1[left];
+            left++;
+            i++;
+        }
+        
+        while (right < n) {
+            res[i] = nums2[right];
+            right++;
+            i++;
+        }
+
+        for (int k = 0; k < m + n; k++) {
+            nums1[k] = res[k];
         }
     }
 }

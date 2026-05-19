19a445daf4473e5d89a1cb6c0f0773e82e037a38
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-05-19 16:07:20 +0000

    Sync LeetCode submission Runtime - 20 ms (83.38%), Memory - 56.8 MB (9.18%)

---
+++ raw-submissions/0917-boats-to-save-people/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0 , right = people.length - 1 , count = 0;
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++; 
+            }
+            right--; 
+            count++; 
+        }
+        return count;
+    }
+}

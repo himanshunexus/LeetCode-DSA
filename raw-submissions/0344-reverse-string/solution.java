1fa22cca683ad5424c1e99fc6e7bc8410a70200f
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-05-19 12:31:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 48 MB (89.02%)

--- raw-submissions/0344-reverse-string/solution.java
+++ raw-submissions/0344-reverse-string/solution.java
@@ -1,15 +1,11 @@
 class Solution {
     public void reverseString(char[] s) {
-        int len = s.length;
-        int i=0;
-        int j=len -1;
-        while(i<j){
-            char temp = s[i];
-            s[i]=s[j];
-            s[j] = temp;
-            i++;
-            j--;
+        int left = 0 , right = s.length - 1;
+        while(left < right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++; right--;
         }
-        
     }
 }

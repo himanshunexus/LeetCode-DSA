3f09f3e7e96ed90a67cbd11fc65ab4f3c6a0ecab
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-05-19 00:52:24 +0000

    Sync LeetCode submission Runtime - 2 ms (89.65%), Memory - 80.5 MB (33.73%)

--- raw-submissions/2634-minimum-common-value/solution.java
+++ raw-submissions/2634-minimum-common-value/solution.java
@@ -1,14 +1,14 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int var = 0;
-        int res=Integer.MAX_VALUE;
-        for(int i=0;i<nums1.length;i++){
-            map.put(nums1[i],map.getOrDefault(nums1[i],0)+1);
-        }
-        for(int num : nums2){
-            if(map.containsKey(num)){
-               return num;
+        int i = 0;
+        int j = 0; 
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                return nums1[i];
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
             }
         }
         return -1;

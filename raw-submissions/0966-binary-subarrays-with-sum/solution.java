17e3f0e35ecdc17f01207c71d4594c8e92bd4cfa
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-05-21 03:24:39 +0000

    Sync LeetCode submission Runtime - 23 ms (33.50%), Memory - 52.7 MB (19.40%)

---
+++ raw-submissions/0966-binary-subarrays-with-sum/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        int prefixSum = 0 , count = 0;
+        for(int num : nums){
+            prefixSum += num;
+            if(map.containsKey(prefixSum - goal)){
+                count += map.get(prefixSum - goal);
+            }
+            map.put(prefixSum , map.getOrDefault(prefixSum , 0) + 1);
+        }
+        return count;
+    }
+}

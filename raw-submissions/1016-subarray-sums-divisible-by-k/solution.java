3324d92993899f4057eccf63be52afeb3fc92dec
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-05-22 12:21:17 +0000

    Sync LeetCode submission Runtime - 3 ms (99.82%), Memory - 48.3 MB (99.73%)

--- raw-submissions/1016-subarray-sums-divisible-by-k/solution.java
+++ raw-submissions/1016-subarray-sums-divisible-by-k/solution.java
@@ -1,17 +1,25 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        long count = 0;
-        int n = nums.length;
+
         int[] freq = new int[k];
+        int prefixSum = 0;
+        int count = 0;
+
         freq[0] = 1;
-        long sum = 0;
-        
-        for(int i = 0; i < n; i++){
-            sum += nums[i];
-            int rem = (int)((sum % k + k) % k);
+
+        for (int num : nums) {
+            prefixSum += num;
+
+            int rem = prefixSum % k;
+
+            if (rem < 0) {
+                rem += k;
+            }
+
             count += freq[rem];
             freq[rem]++;
         }
-        return (int) count;
+
+        return count;
     }
 }

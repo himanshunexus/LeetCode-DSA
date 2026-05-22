18e6e6d476c2761741cc03b43e0925b32be1e822
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-05-22 00:02:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.1 MB (11.36%)

--- raw-submissions/0033-search-in-rotated-sorted-array/solution.java
+++ raw-submissions/0033-search-in-rotated-sorted-array/solution.java
@@ -1,13 +1,24 @@
 class Solution {
     public int search(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
-        }
-        if (map.containsKey(target)) {
-            return map.get(target); 
-        }
-        return -1;
+       int low = 0 , high = nums.length - 1;
+       while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target) return mid;
+            if (nums[low] <= nums[mid]) {
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } 
+            else {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+       }
+       return -1;
     }
 }
-

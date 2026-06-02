6916cbab4a6041c937ea6efb2b45e4d600faa07a
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-06-02 02:58:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.8 MB (35.01%)

--- raw-submissions/0048-rotate-image/solution.java
+++ raw-submissions/0048-rotate-image/solution.java
@@ -1,21 +1,21 @@
 class Solution {
-    public void rotate(int[][] matrix) {
-        for(int i =1;i<matrix.length;i++){
-            for(int j=0;j<i;j++){
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+    public void rotate(int[][] mat) {
+        int n = mat.length , m = mat[0].length;
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < i; j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
             }
         }
 
-        for(int i =0;i<matrix.length;i++){
-            int left = 0, right =matrix[0].length-1;
-            while(left<right){
-                int temp = matrix[i][left];
-                matrix[i][left] = matrix[i][right];
-                matrix[i][right] = temp;
-                left++;
-                right--;
+        for(int i = 0; i < n; i++){
+            int left = 0 , right = m - 1;
+            while(left < right){
+                int temp =  mat[i][left];
+                mat[i][left] = mat[i][right];
+                mat[i][right] = temp;
+                left++;  right--;
             }
         }
     }

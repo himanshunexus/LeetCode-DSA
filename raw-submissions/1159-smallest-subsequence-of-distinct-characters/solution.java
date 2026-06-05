3992058018267fa03f7fcc1bfc846be3fbc555cb
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-06-05 13:23:27 +0000

    Sync LeetCode submission Runtime - 3 ms (61.65%), Memory - 42.7 MB (78.34%)

---
+++ raw-submissions/1159-smallest-subsequence-of-distinct-characters/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String smallestSubsequence(String s) {
+        int[] lastIdx = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            lastIdx[s.charAt(i) - 'a'] = i;
+        }
+        
+        boolean[] seen = new boolean[26];
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (seen[c - 'a']) continue;
+            while (!stack.isEmpty() && stack.peek() > c && lastIdx[stack.peek() - 'a'] > i) {
+                seen[stack.pop() - 'a'] = false;
+            }
+            
+            stack.push(c);
+            seen[c - 'a'] = true;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (char ch : stack) {
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+}

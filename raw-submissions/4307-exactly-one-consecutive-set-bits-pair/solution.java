09b276e195d6ef4dc45d5eddfde6132441932ad7
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-06-06 14:34:06 +0000

    Sync LeetCode submission Runtime - 1 ms (99.33%), Memory - 42.7 MB (41.49%)

---
+++ raw-submissions/4307-exactly-one-consecutive-set-bits-pair/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean consecutiveSetBits(int n) {
+        int count = 0;
+        while(n > 1){
+            if((n & 3) == 3) 
+                count++;
+            n >>= 1;
+        }
+        return count == 1;
+    }
+}

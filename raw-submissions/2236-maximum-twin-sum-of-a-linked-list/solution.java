7e74254a45adf41b80b55cf9efc29cd5cb79f418
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-06-14 03:30:14 +0000

    Sync LeetCode submission Runtime - 4 ms (97.00%), Memory - 101.3 MB (57.24%)

--- raw-submissions/2236-maximum-twin-sum-of-a-linked-list/solution.java
+++ raw-submissions/2236-maximum-twin-sum-of-a-linked-list/solution.java
@@ -1,33 +1,32 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public int pairSum(ListNode head) {
-        ArrayList<Integer> list = new ArrayList<>();
-        ListNode curr =head;
-        while(curr!=null){
-            list.add(curr.val);
-            curr = curr.next;
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        int len = list.size()-1;
-        int i=0;
-        int j=len;
-        int sum=0;
-        int maxSum = 0;
-
-        while(i<j){
-            sum = list.get(i) + list.get(j);
-            maxSum = Math.max(maxSum,sum);
-            i++;
-            j--;
+        
+        ListNode prev = null;
+        ListNode curr = slow;
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
         }
-        return maxSum;
+        
+        int maxVal = 0;
+        ListNode firstHalf = head;
+        ListNode secondHalf = prev;
+        
+        while (secondHalf != null) {
+            maxVal = Math.max(maxVal, firstHalf.val + secondHalf.val);
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+        
+        return maxVal;
     }
 }

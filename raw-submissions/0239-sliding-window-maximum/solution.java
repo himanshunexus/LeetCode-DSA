795e56ed2f0769248ffcdf0de494d424ac883ea7
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-06-18 05:46:34 +0000

    Sync LeetCode submission Runtime - 83 ms (7.49%), Memory - 158.5 MB (6.82%)

--- raw-submissions/0239-sliding-window-maximum/solution.java
+++ raw-submissions/0239-sliding-window-maximum/solution.java
@@ -1,25 +1,22 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
-        int[] ans = new int[n - k + 1];
-        Deque<Integer> dq = new ArrayDeque<>();
-        int idx = 0;
-        for(int i = 0; i < n; i++) {
-            while(!dq.isEmpty() && dq.peekFirst() <= i - k)
-                dq.pollFirst();
-            while(!dq.isEmpty() && nums[dq.peekLast()] <= nums[i])
-                dq.pollLast();
-            dq.addLast(i);
-            if(i >= k - 1)
-                ans[idx++] = nums[dq.peekFirst()];
+        int max[] = new int[n - k + 1];
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        
+        for (int i = 0; i < n; i++) {
+            pq.add(new int[]{nums[i], i});
+            
+            while (pq.peek()[1] <= i - k) {
+                pq.poll();
+            }
+            
+            if (i >= k - 1) {
+                max[i - k + 1] = pq.peek()[0];
+            }
         }
-        return ans;
+        
+        return max;
     }
 }
-
-
-
-
-
-
-

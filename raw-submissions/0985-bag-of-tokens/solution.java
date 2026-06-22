0e8dda54cbd6034f2bc10224e6355c694a63f30f
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-06-22 16:52:42 +0000

    Sync LeetCode submission Runtime - 5 ms (99.74%), Memory - 45.5 MB (7.92%)

---
+++ raw-submissions/0985-bag-of-tokens/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int score = 0 , ans = 0;
+        int left = 0 , right = tokens.length - 1;
+
+        while(left <= right){
+            if(power >= tokens[left]){
+                power -= tokens[left];
+                score++;
+                ans = Math.max(ans , score);
+                left++;
+            }
+            else if(score > 0){
+                power += tokens[right];
+                score--;
+                right--;
+            }
+            else{
+                break;
+            }
+        }
+        return ans;
+    }
+}

e745b7c13d30049d3141ad11235c5e7f654df9c2
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-07-03 15:37:33 +0000

    Sync LeetCode submission Runtime - 3 ms (95.43%), Memory - 43.4 MB (84.19%)

---
+++ raw-submissions/0299-bulls-and-cows/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String getHint(String secret, String guess) {
+        int bulls = 0, cows = 0;
+        int[] counts = new int[10];
+        for (int i = 0; i < secret.length(); i++) {
+            int s = secret.charAt(i) - '0';
+            int g = guess.charAt(i) - '0';
+            if (s == g) {
+                bulls++;
+            } else {
+                if (counts[s]++ < 0) cows++;
+                if (counts[g]-- > 0) cows++;
+            }
+        }
+        return bulls + "A" + cows + "B";
+    }
+}

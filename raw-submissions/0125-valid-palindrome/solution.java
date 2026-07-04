74f83b04a2cecb99f8c931fe1766c1c2318e3477
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-07-04 07:44:50 +0000

    Sync LeetCode submission Runtime - 2 ms (99.32%), Memory - 44 MB (96.54%)

--- raw-submissions/0125-valid-palindrome/solution.java
+++ raw-submissions/0125-valid-palindrome/solution.java
@@ -1,29 +1,26 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
-        while (left < right) {
+        int n = s.length();
+        int left = 0 , right = n - 1;
+        while(left < right){
             char l = s.charAt(left);
             char r = s.charAt(right);
-            if (!isValid(l)) {
-                left++;
-            } else if (!isValid(r)) {
-                right--;
-            } else {
-                if (l != r && !isMatch(l, r)) {
-                    return false;
-                }
+            if(!isValid(l)) left++;
+            else if(!isValid(r)) right--;
+            else{
+                if((l != r) && (!isMatch(l , r))) return false;
                 left++;
                 right--;
             }
         }
         return true;
     }
-    private boolean isValid(char c) {
-        return (c >= 48 && c <= 57) || (c >= 65 && c <= 90) || (c >= 97 && c <= 122);
+    private static boolean isValid(char c){
+        return (c >= 48 && c <= 57 || c >= 65 && c <= 90 || c >= 97 && c <= 122);
     }
-    private boolean isMatch(char c1, char c2) {
-        if (c1 >= 65 && c1 <= 90) c1 += 32;
-        if (c2 >= 65 && c2 <= 90) c2 += 32;
-        return c1 == c2;
+    private static boolean isMatch(char l , char r){
+        if (l >= 65 && l <= 90) l += 32;
+        if (r >= 65 && r <= 90) r += 32;
+        return l == r;
     }
 }

96237162fb570f9d641c3f85158cdda2fa00fce2
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-07-05 14:18:20 +0000

    Sync LeetCode submission Runtime - 2 ms (27.57%), Memory - 45.4 MB (49.41%)

--- raw-submissions/0443-string-compression/solution.java
+++ raw-submissions/0443-string-compression/solution.java
@@ -1,22 +1,22 @@
 class Solution {
     public int compress(char[] chars) {
-        int index = 0; 
-        int i = 0;   
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
         while (i < chars.length) {
             char currentChar = chars[i];
             int count = 0;
             while (i < chars.length && chars[i] == currentChar) {
-                i++;
                 count++;
+                i++;
             }
-            chars[index++] = currentChar;
+            sb.append(currentChar);
             if (count > 1) {
-                String countStr = String.valueOf(count);
-                for (char c : countStr.toCharArray()) {
-                    chars[index++] = c;
-                }
+                sb.append(count);
             }
         }
-        return index;
+        for (int j = 0; j < sb.length(); j++) {
+            chars[j] = sb.charAt(j);
+        }
+        return sb.length();
     }
 }

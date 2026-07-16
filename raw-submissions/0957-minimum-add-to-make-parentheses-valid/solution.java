0faca55dbb88989411c3f3833f17b9c65e6aeb19
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-07-16 02:49:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.8 MB (77.63%)

--- raw-submissions/0957-minimum-add-to-make-parentheses-valid/solution.java
+++ raw-submissions/0957-minimum-add-to-make-parentheses-valid/solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int minAddToMakeValid(String s) {
         char[] arr = s.toCharArray();
         int countA = 0 , countB = 0;
-        StringBuilder sb = new StringBuilder();
+
         for(int i = 0; i < arr.length; i++){
             if(arr[i] == '(') {
                 countA++;
@@ -10,12 +10,11 @@ class Solution {
             else if(arr[i] == ')') {
                 if(countA > 0) countA--;
                 else{
-                    sb.append(arr[i]);
+                    countB++;
                 }
             }
         }
-        int extra = sb.length();
-        int res =  countA + extra;
+        int res =  countA + countB;
         return res;
     }
 }

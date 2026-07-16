51572e0457f8314edc8f4c264cd9197acbc5d985
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-07-16 06:57:58 +0000

    Sync LeetCode submission Runtime - 55 ms (72.14%), Memory - 108 MB (71.04%)

---
+++ raw-submissions/4242-sum-of-gcd-of-formed-pairs/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public static int gcd(int a , int b){
+        while(b != 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    public long gcdSum(int[] nums) {
+        int n = nums.length;
+        int[] prefixGcd = new int[n];
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++){
+            max = Math.max(max , nums[i]);
+            prefixGcd[i] = gcd(nums[i] , max);
+        }
+        Arrays.sort(prefixGcd);
+        int left = 0 , right = n - 1;
+        long  ans = 0;
+        while(left < right){
+            ans += gcd(prefixGcd[left] , prefixGcd[right]);
+            left++;
+            right--;
+        }
+        return ans;
+    }
+}

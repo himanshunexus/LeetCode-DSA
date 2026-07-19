7ea45fb94061620dc963ee6ce958e763744b319c
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-07-19 09:53:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.7 MB (78.77%)

--- raw-submissions/0213-house-robber-ii/solution.java
+++ raw-submissions/0213-house-robber-ii/solution.java
@@ -1,26 +1,20 @@
 class Solution {
+    public int amt(int[] nums, int[] dp, int i, int start) {
+        if (i < start) return 0;
+        if (dp[i] != -1) return dp[i];
+        return dp[i] = Math.max(nums[i] + amt(nums, dp, i - 2, start), amt(nums, dp, i - 1, start));
+    }
     public int rob(int[] nums) {
         int n = nums.length;
-        if(n==0) return 0;
-        if(n==1) return nums[0];
-        if(n==2) return Math.max(nums[0], nums[1]);
-        int[] res = new int[n];
-        res[0] = nums[0];
-        res[1] = nums[1];
-        
-        res[1] = Math.max(nums[0], nums[1]);
-        for(int i = 2; i < n-1; i++){
-            res[i] = Math.max(nums[i] + res[i-2], res[i-1]);
-        }
-        int case1 = res[n-2];
-        
-        res[1] = nums[1];
-        res[2] = Math.max(nums[1], nums[2]);
-        for(int i = 3; i < n; i++){
-            res[i] = Math.max(nums[i] + res[i-2], res[i-1]);
-        }
-        int case2 = res[n-1];
-        
-        return Math.max(case1, case2);
+        if (n == 1) return nums[0];
+        int[] dp1 = new int[n];
+        Arrays.fill(dp1, -1);
+        int case1 = amt(nums, dp1, n - 2, 0);
+
+        int[] dp2 = new int[n];
+        Arrays.fill(dp2, -1);
+        int case2 = amt(nums, dp2, n - 1, 1);
+
+        return Math.max(case1 , case2);
     }
 }

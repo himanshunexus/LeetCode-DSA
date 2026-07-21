51a44ba103dce3b93059b8643ea404de4544d95b
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-07-21 07:27:29 +0000

    Sync LeetCode submission Runtime - 7 ms (46.33%), Memory - 55.9 MB (18.36%)

--- raw-submissions/0169-majority-element/solution.java
+++ raw-submissions/0169-majority-element/solution.java
@@ -1,17 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int candidate = nums[0] , count = 0 , n = nums.length;
-        for(int num : nums){
-            if(count == 0) 
-                candidate = num;
-            count += (num == candidate) ? 1 : -1;
-        }
-        int freq = 0;
-        for(int num : nums){
-            if(num == candidate){
-                freq++;
-            }
-        }
-        return (freq > n / 2) ? candidate : -1;
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
     }
 }

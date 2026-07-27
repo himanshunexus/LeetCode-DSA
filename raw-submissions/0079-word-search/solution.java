5929256abe36d16e72058eb48648979334c36557
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-07-27 13:08:57 +0000

    Sync LeetCode submission Runtime - 261 ms (8.51%), Memory - 46.9 MB (7.81%)

--- raw-submissions/0079-word-search/solution.java
+++ raw-submissions/0079-word-search/solution.java
@@ -1,20 +1,35 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int m = board.length, n = board[0].length;
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                if (dfs(board, word, i, j, 0)) return true;
+        int m = board.length;
+        int n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    if (find(board, i, j, 0, word)) return true;
+                }
+            }
+        }
         return false;
     }
 
-    boolean dfs(char[][] board, String word, int i, int j, int k) {
-        if (k == word.length()) return true;
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(k)) return false;
-        char tmp = board[i][j];
-        board[i][j] = '#';
-        boolean found = dfs(board, word, i+1, j, k+1) || dfs(board, word, i-1, j, k+1) ||
-                        dfs(board, word, i, j+1, k+1) || dfs(board, word, i, j-1, k+1);
-        board[i][j] = tmp;
-        return found;
+    private boolean find(char[][] board, int i, int j, int idx, String word) {
+        if (idx == word.length()) return true;
+
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(idx)) {
+            return false;
+        }
+
+        char temp = board[i][j];
+        board[i][j] = '$';
+
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+        for (int[] dir : directions) {
+            if (find(board, i + dir[0], j + dir[1], idx + 1, word)) return true;
+        }
+
+        board[i][j] = temp;
+        return false;
     }
 }

8d574e28fc073078ad4358463e8e5b35bdb3afd7
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-07-27 08:15:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.7 MB (67.22%)

--- raw-submissions/0035-search-insert-position/solution.java
+++ raw-submissions/0035-search-insert-position/solution.java
@@ -1,19 +1,7 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target)
-                return mid;
-            else if (nums[mid] < target)
-                left = mid + 1;
-            else
-                right = mid - 1;
-        }
-
-        return left;
+        int res = Arrays.binarySearch(nums, target);
+        if(res >= 0) return res;
+        else return -(res + 1);
     }
 }
-

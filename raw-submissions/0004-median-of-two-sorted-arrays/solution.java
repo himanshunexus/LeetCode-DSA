11dd2c109044653bb7c33c1bda0af602163dd369
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-07-27 03:56:11 +0000

    Sync LeetCode submission Runtime - 8 ms (13.94%), Memory - 49.2 MB (5.12%)

--- raw-submissions/0004-median-of-two-sorted-arrays/solution.java
+++ raw-submissions/0004-median-of-two-sorted-arrays/solution.java
@@ -1,37 +1,21 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums1.length > nums2.length) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
-        
-        int m = nums1.length, n = nums2.length;
-        int totalLeft = (m + n + 1) / 2;
-        
-        int left = 0, right = m;
-        while (left <= right) {
-            int i = (left + right) / 2;
-            int j = totalLeft - i;
-            
-            int nums1LeftMax = (i == 0) ? Integer.MIN_VALUE : nums1[i - 1];
-            int nums1RightMin = (i == m) ? Integer.MAX_VALUE : nums1[i];
-            int nums2LeftMax = (j == 0) ? Integer.MIN_VALUE : nums2[j - 1];
-            int nums2RightMin = (j == n) ? Integer.MAX_VALUE : nums2[j];
-            
-            if (nums1LeftMax <= nums2RightMin && nums2LeftMax <= nums1RightMin) {
-                if ((m + n) % 2 == 1) {
-                    return Math.max(nums1LeftMax, nums2LeftMax);
-                } else {
-                    return (Math.max(nums1LeftMax, nums2LeftMax) +
-                            Math.min(nums1RightMin, nums2RightMin)) / 2.0;
-                }
-            } else if (nums1LeftMax > nums2RightMin) {
-                right = i - 1;
-            } else {
-                left = i + 1;
+        double[] ans = new double[nums1.length + nums2.length];
+        for(int i = 0;i < nums1.length + nums2.length; i++){
+            if(i < nums1.length){
+                ans[i] = nums1[i];
+            }
+            else{
+                ans[i] = nums2[(i - nums1.length)];
             }
         }
-        
-        throw new IllegalArgumentException();
+       Arrays.sort(ans);
+      
+        if(ans.length % 2 == 0){
+            return (ans[ans.length / 2] + ans[((ans.length / 2) - 1)]) / 2;
+        }
+        else{
+           return ans[((ans.length) - 1) / 2];
+        }
     }
 }
-

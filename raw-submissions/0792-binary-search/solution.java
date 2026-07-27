69986601a6c92867f46f79def5a67673d3e53a7f
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-07-27 08:13:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 48.1 MB (85.83%)

--- raw-submissions/0792-binary-search/solution.java
+++ raw-submissions/0792-binary-search/solution.java
@@ -1,17 +1,6 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length-1;
-
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            if(nums[mid]==target)
-                return mid;
-            else if(nums[mid]< target)
-                left=mid+1;
-            else 
-                right = mid-1;
-        }
-        return -1;
+        int res = Arrays.binarySearch(nums, target);
+        return res >= 0 ? res : -1;
     }
 }

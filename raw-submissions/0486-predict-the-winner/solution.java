93d938a037d875f0be4654419f233f9d027b6dd9
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-08-01 03:01:23 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.9 MB (18.49%)

---
+++ raw-submissions/0486-predict-the-winner/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        Integer[][] memo = new Integer[n][n];
+        return maxDiff(nums, 0, n - 1, memo) >= 0;
+    }
+    private int maxDiff(int[] nums, int i, int j, Integer[][] memo) {
+        if (i == j) return nums[i];
+        if (memo[i][j] != null) return memo[i][j];
+
+        int pickStart = nums[i] - maxDiff(nums, i + 1, j, memo);
+        int pickEnd = nums[j] - maxDiff(nums, i, j - 1, memo);
+
+        return memo[i][j] = Math.max(pickStart, pickEnd);
+    }
+}

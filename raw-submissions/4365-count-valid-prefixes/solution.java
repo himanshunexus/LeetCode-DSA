880929027958ae260da85578f1ee1930dcb94197
HIMANSHU GOSWAMI <himanshugoswami5060@gmail.com>
2026-08-01 14:36:25 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 43.9 MB (60.03%)

---
+++ raw-submissions/4365-count-valid-prefixes/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countValidPrefixes(String s) {
+        int count0 = 0;
+        int count1 = 0;
+        int ans = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '0') count0++;
+            else if(ch == '1') count1++;
+            if(Math.abs(count0 - count1) <= 1) ans++;
+        }
+        return ans;
+    }
+}
